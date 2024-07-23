ed509b2c0dbf8001870f479e6e6b9d9e59a0d49b
artem-alekseenko <ivanovo.cwi@gmail.com>
2024-07-23 20:14:30 +0300

    add auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt"],
+  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "@nuxtjs/supabase"],
+  supabase: {
+    redirectOptions: {
+      login: "/login",
+      callback: "/confirm",
+      exclude: ["/*"],
+    },
+  },
 
   future: {
     compatibilityVersion: 4,
