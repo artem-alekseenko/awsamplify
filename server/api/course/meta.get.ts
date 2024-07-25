2b5abe09fb25258d48b292c27e88c4bd99848c09
artem-alekseenko <ivanovo.cwi@gmail.com>
2024-07-25 21:45:34 +0300

    add useFetch and cache

--- server/api/course/meta.get.ts
+++ server/api/course/meta.get.ts
@@ -1,28 +1,15 @@
-import { Course, Chapter } from "~/types/course";
-import courseData from "~/server/courseData";
+import {
+  Course,
+  Chapter,
+  CourseMeta,
+  OutlineChapter,
+  OutlineLesson,
+} from "~/types/course";
+import course from "~/server/courseData";
 
-const course: Course = courseData;
+course as Course;
 
-type OutlineBase = {
-  title: string;
-  slug: string;
-  number: number;
-};
-
-type OutlineChapter = OutlineBase & {
-  lessons: OutlineLesson[];
-};
-
-type OutlineLesson = OutlineBase & {
-  path: string;
-};
-
-type CourseMeta = {
-  title: string;
-  chapters: OutlineChapter[];
-};
-
-export default defineEventHandler((): CourseMeta => {
+export default defineEventHandler((event): CourseMeta => {
   const outline: OutlineChapter[] = course.chapters.reduce(
     (prev: OutlineChapter[], next: Chapter) => {
       const lessons: OutlineLesson[] = next.lessons.map((lesson) => ({
